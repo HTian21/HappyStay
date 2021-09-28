c2af22bf1608bc2e03aba811df30dd1a6e26ce7d
Tim <91568343+HTian21@users.noreply.github.com>
2021-09-28 16:38:38 -0500

    Create StayDeleteException.java

---
+++ HappyStay/exception/StayDeleteException.java
@@ -0,0 +1,7 @@
+package com.laioffer.happystay.exception;
+
+public class StayDeleteException extends RuntimeException {
+    public StayDeleteException(String message) {
+        super(message);
+    }
+}
