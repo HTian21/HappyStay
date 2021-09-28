236b6fbc9765eb866149f2fbd48283999144e8a1
Tim <91568343+HTian21@users.noreply.github.com>
2021-09-28 16:36:13 -0500

    Create GCSUploadException.java

---
+++ HappyStay/exception/GCSUploadException.java
@@ -0,0 +1,7 @@
+package com.laioffer.happystay.exception;
+
+public class GCSUploadException extends RuntimeException {
+    public GCSUploadException(String message) {
+        super(message);
+    }
+}
